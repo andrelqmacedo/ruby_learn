68e9eec5efac92995d87c12af87f239f0f8653d0
Andre Queiroga <andre.queiroga@see.pb.gov.br>
2023-08-30 13:17:31 -0300

    puts and escape characters

---
+++ learn-to-code-with-ruby/02 - Getting Started/escape_characters.rb
@@ -0,0 +1,8 @@
+puts "some text\nmore text" #/n stands for new line
+puts "\tOnce upon a time..." #\t stands for tab, i.e., identation
+
+puts "Juliet said \"Goodbye\" to Romeo" #\" permits to use double quotes inside strings
+puts 'Juliet said \'Goodbye\' to Romeo' 
+
+
+
---
+++ learn-to-code-with-ruby/02 - Getting Started/the_puts_method.rb
@@ -0,0 +1,16 @@
+puts 1
+puts 1 + 3
+
+#puts invokes a line breaker after the output - "\n"
+puts #just print nothing and add a line break
+
+puts (12*5)
+
+#The method puts, as other methods, could be called with parenthesis
+
+puts "Hello, my name is Andre"
+
+puts 3.14159
+
+puts 5, 3, 5
+#puts with multiple values separeted from a comma outputs the values with a line breaker between the values
