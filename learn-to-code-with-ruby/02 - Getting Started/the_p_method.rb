25bba4ea99462d19bddbc9419df8d42c6d80bda9
Andre Queiroga <andre.queiroga@see.pb.gov.br>
2023-08-30 13:33:00 -0300

    adds p and print methods

---
+++ learn-to-code-with-ruby/02 - Getting Started/the_p_method.rb
@@ -0,0 +1,15 @@
+puts "Steven Seagal"
+
+p "Steven Seagal" 
+#the p method exists more for the benefit of the developer
+#'cause it gives additional detail about the content being outputed.
+
+puts 5
+puts "5"
+
+p 5
+p "5"
+
+puts "Hi there\nline break"
+p "Hi there\nline break" #we see the original \n
+
