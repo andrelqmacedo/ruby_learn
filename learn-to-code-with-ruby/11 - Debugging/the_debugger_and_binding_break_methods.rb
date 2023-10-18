898491f6fc8a7aa094292566ec78aedae2cc4741
Andre Queiroga <andre.queiroga@see.pb.gov.br>
2023-10-18 14:05:35 -0300

    starts debbuging studies

---
+++ learn-to-code-with-ruby/11 - Debugging/the_debugger_and_binding_break_methods.rb
@@ -0,0 +1,30 @@
+require "debug"
+
+#Standard library - Collection of code organized in files called "librarys"
+
+# DateTime library - helps with..dates and times
+
+candy = "Sour Patch Kids"
+puts "I love eating #{candy}"
+
+debugger #breakpoint
+
+beverage = "Arizona Iced Tea"
+puts "I love drinking #{beverage}"
+
+debugger #breakpoint
+
+puts "This is the end of the program"
+
+
+# To make the code run after the break point we must type "continue" or just "c" in the terminal
+
+
+# As an alternative to the debugger, we can use another method which is the break method on the binding object
+# debugger = binding.break :
+# # candy = "Sour Patch Kids"
+# puts "I love eating #{candy}"
+
+# biding.break                     => It'll work the exact same way as the debugger
+
+# beverage = "Arizona Iced Tea"
