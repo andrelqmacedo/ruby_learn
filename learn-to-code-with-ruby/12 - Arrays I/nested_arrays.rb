93e20b2041cd230b4fb2431fd42672020ef594bf
Andre Queiroga <andre.queiroga@see.pb.gov.br>
2023-10-24 10:01:13 -0300

    continues loops studies

---
+++ learn-to-code-with-ruby/12 - Arrays I/nested_arrays.rb
@@ -0,0 +1,19 @@
+# Nested Arrays - Arrays that hold other array within them
+#
+# Column A, Column B
+# Row Value, Row Value
+#
+
+spreadsheet = [
+  ["Student", "Class", "Grade"],
+  ["Sally", "Computer Science", "95"],
+  ["Ingrid", "Physics", 45],
+]
+
+p spreadsheet
+
+first_row, second_row, third_row = spreadsheet
+
+p first_row
+p second_row
+p third_row
