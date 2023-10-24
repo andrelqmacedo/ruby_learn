1785137b22f7095b2ec50cc8d9c021f41274d85b
Andre Queiroga <andre.queiroga@see.pb.gov.br>
2023-10-24 10:08:33 -0300

    continues arrays studies - shortcuts

---
+++ learn-to-code-with-ruby/12 - Arrays I/shorthand_%w_syntax_to_create_an_array_of_strings.rb
@@ -0,0 +1,10 @@
+# Shortcut to array of Strings - %W[] or %w[]
+
+names = ["Jack", "Jill", "John", "James"]
+p names
+
+
+# When we use this shortcut syntax, we dont need quotes for the strings and dont need commas between the elements
+
+names = %w[Jack Jill John James]
+p names
