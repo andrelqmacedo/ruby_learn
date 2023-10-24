7645d4f5482e1e5f7eb851e182be5829292267e7
Andre Queiroga <andre.queiroga@see.pb.gov.br>
2023-10-24 09:30:11 -0300

    adds initial arrays studies

---
+++ learn-to-code-with-ruby/12 - Arrays I/intro_to_arrays.rb
@@ -0,0 +1,24 @@
+# An array is an ordered list of objects, ordered sequence of items
+#
+# Arrays are created with a pair of square brackts
+#
+# We often use the word "elements" to describe the itens
+#inside the array
+#
+# The array itself is an object.
+# Think of a box in the real world
+# A box can hold multiple itens inside of it.
+# The box is an object and the items inside of ir are objects as well
+
+numbers = [4, 8, 15, 16, 23, 42]
+
+p numbers
+
+# Typically with arrays, we´ll use the 'p' method, 'cause the 'puts' method will only print the elements
+# Arrays can be consisting of differente data types
+
+things = [4, 5.6, "Gagá", true, 6, false]
+toys = ["Teddy bear", "Water gun", "Board game"]
+registrations = [true, true, false, true, false]
+
+p registrations
