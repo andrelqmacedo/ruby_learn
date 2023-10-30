32fb86496efb2ccba55884290a44f54e1c551fd2
Andre Queiroga <andre.queiroga@see.pb.gov.br>
2023-10-30 09:58:08 -0300

    expl of each within each

---
+++ learn-to-code-with-ruby/14 - Arrays II/each_within_each.rb
@@ -0,0 +1,10 @@
+# How to nest an each loop within another each loop
+
+shirts = ["striped", "plain white", "plaid", "band"]
+ties = ["polka dot", "solid color", "boring"]
+
+shirts.each do |shirt|
+  ties.each do |tie|
+    puts "Option: A #{shirt} shirt and #{tie} tie."
+  end
+end
