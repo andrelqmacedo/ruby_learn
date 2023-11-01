8ae2de6ae588b0ed9d089568edabff60469672cb
Andre Queiroga <andre.queiroga@see.pb.gov.br>
2023-11-01 09:23:15 -0300

    adds any? and all? methods

---
+++ learn-to-code-with-ruby/14 - Arrays II/the_any?_and_all?_methdos.rb
@@ -0,0 +1,13 @@
+# any? method - check if any array element satisfies a condition
+# all? method - check if every array element satisfies a condition
+
+# Both are predicated methods that accept a block;
+
+sports = ["Football", "Basketball", "Baseball", "Tennis", "Golf"]
+
+p sports.any? { |sport| sport.length == 8 } #true
+p sports.any? { |sport| sport.length == 12 } #false
+# The any? method is looking for just one match, if ONE element satisfies the condition, the any? method will return true
+
+p sports.all? {|sport| sports.length < 100 }
+# It's true that all elements in the sport array have less than 100 charecters
