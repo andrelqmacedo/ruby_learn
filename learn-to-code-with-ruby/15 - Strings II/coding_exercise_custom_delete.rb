dd28b145aa34e4e1229276ca6c1eef0525472fe2
Andre Queiroga <andre.queiroga@see.pb.gov.br>
2023-11-06 15:02:35 -0300

    custom delete exercise

---
+++ learn-to-code-with-ruby/15 - Strings II/coding_exercise_custom_delete.rb
@@ -0,0 +1,28 @@
+# Define a custom_delete method that accepts a text string and
+# a string of deletion characters. The method should build up
+# a new string consisting of only characters that are NOT found
+# among the deletion characters. Do not use the built-in
+# delete method in your solution.
+#
+# Examples:
+# The => indicates the expected return value
+# custom_delete("cottage cheese", "c")     => "ottage heese"
+# custom_delete("cottage cheese", "e")     => "cottag chs"
+# custom_delete("cottage cheese", "ce")    => "ottag hs"
+# custom_delete("cottage cheese", "ec")    => "ottag hs"
+
+
+def custom_delete(text, del_chars)
+  result = ""
+  text.each_char do |char|
+    unless del_chars.include?(char)
+      result << char
+    end
+  end
+  result
+end
+
+p custom_delete("cottage cheese", "c")     #=> "ottage heese"
+p custom_delete("cottage cheese", "e")     #=> "cottag chs"
+p custom_delete("cottage cheese", "ce")    #=> "ottag hs"
+p custom_delete("cottage cheese", "ec")    #=> "ottag hs"
