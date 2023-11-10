7048eadcf0803a0c59c0591d9652d328baedb42d
Andre Queiroga <andre.queiroga@see.pb.gov.br>
2023-11-10 15:23:18 -0300

    adds coding exercise - hashes

---
+++ learn-to-code-with-ruby/17 - Hashes/coding_exercise_word_frequency.rb
@@ -0,0 +1,34 @@
+# Define a word_frequency method that accepts a piece of text.
+# Return a hash with a count of the number of times each word
+# appears within the text. The hash keys should be the words
+# and the values should be their counts. Assume the text will
+# be in all lowercase.
+#
+# Examples:
+# The => indicates the expected return value
+# word_frequency("blue red blue green")  => {"blue"=>2, "red"=>1, "green"=>1}
+# word_frequency("a land far far away")  => {"a"=>1, "land"=>1, "far"=>2, "away"=>1}
+# word_frequency("")                     => {}
+
+# def word_frequency(text)
+#   hash_to_return = {}
+#   words_array = text.split(" ")
+#   words_array.each do |word|
+#     if hash_to_return[word].nil?
+#       hash_to_return[word] = 1
+#     else
+#       hash_to_return[word] = hash_to_return[word] + 1
+#     end
+#   end
+#   hash_to_return
+# end
+
+def word_frequency(text)
+  hash_to_return = Hash.new(0)
+  words_array = text.split(" ")
+  words_array.each { |word| hash_to_return[word] += 1 }
+  hash_to_return
+end
+p word_frequency("blue red blue green")  #=> {"blue"=>2, "red"=>1, "green"=>1}
+p word_frequency("a land far far away")  #=> {"a"=>1, "land"=>1, "far"=>2, "away"=>1}
+p word_frequency("")                     #=> {}
