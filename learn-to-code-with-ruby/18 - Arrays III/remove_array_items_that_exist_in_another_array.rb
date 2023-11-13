7a4af772f66dbda50ee3af7510fa1cba5cf389ad
Andre Queiroga <andre.queiroga@see.pb.gov.br>
2023-11-13 10:31:03 -0300

    removing elements

---
+++ learn-to-code-with-ruby/18 - Arrays III/remove_array_items_that_exist_in_another_array.rb
@@ -0,0 +1,9 @@
+# - (minus) sign - removes all array elements found in the second array, providing a new array consisting of all the elements that are not found
+# in this array
+
+# We provide a starter array, the a minus sign, and then a second array.
+# Any elements that are found in the second array is going to be removed from the first.
+# It's like subtracting a collection of elements from the very first array.
+
+p [1, 1, 2, 2, 3, 3, 3, 4, 5] - [2, 3]
+p [1, 1, 2, 2, 3, 3, 3, 4, 5].-([2, 3])
