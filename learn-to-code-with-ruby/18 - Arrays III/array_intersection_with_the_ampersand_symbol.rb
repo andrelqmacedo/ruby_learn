975fc2b35f3e600cdff7f4889a12261ade7ed927
Andre Queiroga <andre.queiroga@see.pb.gov.br>
2023-11-13 10:45:29 -0300

    combining arrays

---
+++ learn-to-code-with-ruby/18 - Arrays III/array_intersection_with_the_ampersand_symbol.rb
@@ -0,0 +1,12 @@
+# & symbol - combine multiple arrays, keep elements that are found itn both.
+
+# How to create a intersection between two or multiple arrays
+
+p [1, 1, 2, 4, 5,] & [1, 4, 5, 8 ,9]
+
+# It keeps only the elements that are found in both the arragys
+#
+#
+
+
+p [1, 1, 2, 4, 5,],&([1, 4, 5, 8 ,9])
