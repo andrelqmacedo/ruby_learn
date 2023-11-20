bcd2fa2dad7947330eaec669594139bf06174322
Andre Queiroga <andre.queiroga@see.pb.gov.br>
2023-11-20 10:40:50 -0300

    new method on native objects

---
+++ learn-to-code-with-ruby/20 - Classes I/the_new_method_on_ruby's_native_objects.rb
@@ -0,0 +1,9 @@
+p Hash.new(0) # creates a new hash. The argument represents the default value to fall back when the key does not exist
+
+p String.new("Andre") # We're going to get a string from the 'new' method being called on the string class.
+
+p Array.new(3) # the argument represents the number of elements from the array, that´s going to be populated by 'nil' objects
+
+{} #=> instatiates a hash from the Hash class. the curly braces are just shortcuts
+"" #=> instatiates a string from the String class
+[]
