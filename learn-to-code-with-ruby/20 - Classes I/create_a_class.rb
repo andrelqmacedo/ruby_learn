aa46ae64e9e678f419279ebcbdf33c5066c1dd4d
Andre Queiroga <andre.queiroga@see.pb.gov.br>
2023-11-20 10:05:48 -0300

    creates a class

---
+++ learn-to-code-with-ruby/20 - Classes I/create_a_class.rb
@@ -0,0 +1,11 @@
+# UpperCamelCase - Capitalize the first letter of every word.
+
+class Guitar
+  # logic belonging to the class
+end
+
+Guitar.new # instantiate a new Guitar object.
+
+acoustic = Guitar.new
+electric = Guitar.new
+hybrid =    Guitart.nwe
