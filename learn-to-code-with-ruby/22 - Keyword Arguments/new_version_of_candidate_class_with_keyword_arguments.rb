a9cde2ecfc22cf272ee5bcdf175deb436521af09
Andre Queiroga <andre.queiroga@see.pb.gov.br>
2023-11-30 11:04:07 -0300

    coding exercise - keyword arguments

---
+++ learn-to-code-with-ruby/22 - Keyword Arguments/new_version_of_candidate_class_with_keyword_arguments.rb
@@ -0,0 +1,33 @@
+
+class Candidate
+  attr_reader :name, :age, :occupation, :hobby, :birthplace
+
+  def initialize(
+    name:,
+    age:,
+    occupation: "Candidate",
+    hobby: "Running for office",
+    birthplace: "USA"
+    )
+    @name = name
+    @age = age
+    @occupation = occupation
+    @hobby = hobby
+    @birthplace = birthplace
+  end
+
+end
+
+senator = Candidate.new({
+  name: "Mr. Smith",
+  age: 53,
+  occupation: "Banker",
+  hobby: "Fishing",
+  birthplace: "Kentucky"
+})
+
+p senator.name
+p senator.age
+p senator.occupation
+p senator.hobby
+p senator.birthplace
