fb26bf09082f373cc7eb88001c7369fa90856f7f
Andre Queiroga <andre.queiroga@see.pb.gov.br>
2023-12-04 12:25:13 -0300

    super keyword - part 2

--- learn-to-code-with-ruby/23 - Classes III/the_super_keyword_II.rb
+++ learn-to-code-with-ruby/23 - Classes III/the_super_keyword_II.rb
@@ -10,6 +10,11 @@
 #       to the superclass's methods.
 
 class Car
+  attr_reader :maker
+
+  def initialize(maker)
+    @maker = maker
+  end
 
   def drive(speed)
     "Vrum, vrum at #{speed} km/h."
@@ -18,9 +23,34 @@ end
 
 class Firetruck < Car
   def drive(speed)
-    super + " Beep! Beep at #{speed} km/h."             # The 'super' without parentheses passed in the 'speed' argument of 60 to the
-  end                                                   # drive method in the Car class.
+    super + " Beep! Beep at #{speed} km/h."             # The 'super' without parentheses passed in the speed argument of 60 to the
+  end                                                   # drive method in the superclass Car
 end
 
 ft = Firetruck.new
+puts ft.drive(60)
+
+
+class Firetruck2 < Car
+  def drive(speed)
+    super() + " Beep! Beep at #{speed} km/h."           # With parentheses and no argument like so that's how we more explicitly tell Ruby
+  end                                                   # 'do not pass anything up'.
+end
+
+ft2 = Firetruck2.new
+puts ft.drive(60)
+
+class Firetruck3 < Car
+
+  def initialize(maker, sirens)
+    super(maker)                                      # We only pass the 'maker' argument, and the sirens is saved in a attr, because with the
+    @sirens = sirens                                  # parentheses and the argument, we pass only the argument to the superclass
+  end
+
+  def drive(speed)
+    super() + " Beep! Beep at #{speed} km/h."
+  end
+end
+
+ft3 = Firetruck3.new
 puts ft.drive(60)
