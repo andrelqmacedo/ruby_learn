0921cf1ffdc67702421542852fc6af6716d0654e
Andre Queiroga <andre.queiroga@see.pb.gov.br>
2023-12-24 00:40:11 -0300

    anchors]

---
+++ learn-to-code-with-ruby/28 - Regular Expressions/anchors.rb
@@ -0,0 +1,16 @@
+# Anchor - regular expression symbol that signifies a location in the string (beginning or end)
+
+# We can use the anchor to tell the pattern to look specifically at the very beginning or the very end
+# of the string, but nowhere in between
+
+poem = "99 bottles of beer on the wall, 99 bottles of beer"
+
+p poem.scan(/\d+/)
+
+# But what if we only want to pull out the collection of digits at the very beginning of a string?
+
+# We can provide an anchor right before our digit specifier and it's going to look like this:
+
+p poem.scan(/\A\d+/) # The backslash followed by the letter A forms an anchor, and what it degisnates is
+                     # 'look at the verry beginning of the string. connect or bind yourself to the very beginning
+                     # and then, from that point, look for the presence of one or more digits in a row.'
