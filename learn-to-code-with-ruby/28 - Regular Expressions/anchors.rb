5415eb731356e4bb08120f2bce1e40c0b61a9c32
Andre Queiroga <andre.queiroga@see.pb.gov.br>
2023-12-24 00:40:28 -0300

    anchors

--- learn-to-code-with-ruby/28 - Regular Expressions/anchors.rb
+++ learn-to-code-with-ruby/28 - Regular Expressions/anchors.rb
@@ -3,7 +3,7 @@
 # We can use the anchor to tell the pattern to look specifically at the very beginning or the very end
 # of the string, but nowhere in between
 
-poem = "99 bottles of beer on the wall, 99 bottles of beer"
+poem = "99 bottles of beer on the wall, 99 bottles of beer..."
 
 p poem.scan(/\d+/)
 
@@ -14,3 +14,6 @@ p poem.scan(/\d+/)
 p poem.scan(/\A\d+/) # The backslash followed by the letter A forms an anchor, and what it degisnates is
                      # 'look at the verry beginning of the string. connect or bind yourself to the very beginning
                      # and then, from that point, look for the presence of one or more digits in a row.'
+
+
+p poem.scan(/\.\z/)
