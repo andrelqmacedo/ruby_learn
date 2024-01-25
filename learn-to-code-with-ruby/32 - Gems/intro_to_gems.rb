43589c30e474172a4a933f0ba3ee4f118a42d467
AndreQueiroga <alqmacedo@gmail.com>
2024-01-25 12:01:36 -0300

    explain gemfile and bundle

--- learn-to-code-with-ruby/32 - Gems/intro_to_gems.rb
+++ learn-to-code-with-ruby/32 - Gems/intro_to_gems.rb
@@ -28,4 +28,38 @@
 # On the terminal, we can run these commands
       # To verify if the gem program is installed: $ gem -v
       # To update the gem software itself to the latest version: $ gem update --system
-      #
+      # To install a gem: $ gem install <name_of_the_gem>
+
+# Gemfile: it's a special file, which is going to list all of the dependecies of
+# our project. We can rely on this file to download all of our project dependencies,
+# in other words, all of our project's gems, with just one command.
+# The Gemfile is just a plain text file, or essentially a Ruby file, where we declare
+# all of our project's dependencies.
+# In a typical code base, this file is where developers are going to write all
+# of the gems that the project needs, along with a lockdown version number, a family
+# of version numbers.
+# So, if we run this code in the future, and we download all of the dependecies
+# specified in the Gemfile, we're going to be guaranteed to get the right version
+# of the gem rather than whatever happens to be the most up to date version.
+      # At the very top of the Gemfile we're going to specify where we would
+      # like to download our gems from.
+      # For each gem that we  want to add as a dependency for the current project,
+      # we're going to use the word 'gem' and then provide the name of the gem as a String
+                # gem "faker"
+      # The second argument, after the comma, is another string where we
+      # can write the specific version number that we want to declare.
+                # gem "faker", "3.2.2"
+      # Before the version number, in order to add flexibility, we can put
+      # a tilde (~) and a greater then sign (>) and add a space. This
+      # essentially says "this version or any version higher within this level of specificity".
+      # It's important to notice that as we specify the 3 numbers of the version number
+      # (major version, minor version and patch version, respectvlly) the Gemfile is going to search
+      # for anything in this family, or this category of version, as long as it doesn't violate
+      # the major or the minor version. And it works for the minor version number as well:
+                # gem "faker", "~> 3.2.0" - 3.2.1 is valid / 3.2.6 is valid / 3.3.0 is not valid / 4.0.5 is not valid
+                # gem "faker", "~> 3.2" - 3.2 is valid / 3.3 is valid / 3.4.5 is valid / 4.0 is not valid
+
+# The command to download all of the dependencies in the Gemfile is: $ bundle instal
+# Bundle is the program that's going to look for a Gemfile in the current directory,
+# find all of the dependencies, including all of theis sub dependencies, figure out everything
+# we need to dowload and do that all in a single command.
